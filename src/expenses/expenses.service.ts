12bca4c9bbcca8125b9dc76d30e3c8756b090aee
Godwin Emmanuel <goilemma@gmail.com>
2022-03-20 07:40:32 +0100

    Update expenses

--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -26,25 +26,25 @@ export class ExpensesService {
     return this.expensesRepository.findOne(id);
   }
 
-  // async update(id: number, updateExpenseDto: UpdateExpenseDto) {
-  //   const expense = await this.expensesRepository.find({ where: { id } });
-  //
-  //   if (!expense) return { message: 'This operation cannot be performed' };
-  //
-  //   const updatedExpense = {};
-  //
-  //   // Get keys of incoming update and put them in an array named keys
-  //   const keys = Object.keys(updateExpenseDto);
-  //   if (!keys.length) return { message: 'No data to update' };
-  //
-  //   keys.forEach((key) => {
-  //     updatedExpense[key] = updateExpenseDto[key];
-  //   });
-  //
-  //   await this.expensesRepository.update(id, updatedExpense);
-  //
-  //   return { message: 'Expense updated successfully' };
-  // }
+  async update(id: number, updateExpenseDto: UpdateExpenseDto) {
+    const expense = await this.expensesRepository.find({ where: { id } });
+
+    if (!expense) return { message: 'This operation cannot be performed' };
+
+    const updatedExpense = {};
+
+    // Get keys of incoming update and put them in an array named keys
+    const keys = Object.keys(updateExpenseDto);
+    if (!keys.length) return { message: 'No data to update' };
+
+    keys.forEach((key) => {
+      updatedExpense[key] = updateExpenseDto[key];
+    });
+
+    await this.expensesRepository.update(id, updatedExpense);
+
+    return { message: 'Expense updated successfully' };
+  }
 
   remove(id: number) {
     return `This action removes a #${id} expense`;
